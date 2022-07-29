ba6c81a0ad873715790d5288d346dea9e5ff3353
Honn0 <felipesouza.root@gmail.com>
2022-07-28 23:47:33 -0300

    cadastro finalizado

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class cadastro(models.Model):
+    database = models.CharField(max_length=100)
+    table = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('date published')
+    definir = models.CharField(max_length=100)
+    definir = models.CharField(max_length=100)
+    definir = models.CharField(max_length=100)
+    script_conf = models.CharField(max_length=200)
+
+
